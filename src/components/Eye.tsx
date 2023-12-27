2c94d484b116eb4a4736debe18630f1a1fa33568
Diogo Ribeiro <diogo.engsoftware@gmail.com>
2023-12-27 00:02:02 -0300

    update avg views graph styles

--- src/components/Eye.tsx
+++ src/components/Eye.tsx
@@ -3,10 +3,11 @@ import eye from '../assets/eye-icon.png';
 function Eye() {
   return (
     <defs>
-      <pattern id="eye" x="0" y="0" patternUnits="userSpaceOnUse" width="30" height="10">
-        <image href={eye} x="0" y="0" width="30" height="10" />
+      <pattern id="eye" x="0" y="0" patternUnits="userSpaceOnUse" width="30" height="20">
+        <image href={eye} x="0" y="0" width="30" height="20" />
       </pattern>
       <pattern id="bar" x="0" y="0" width="1" height="1">
+        <rect width="100%" height="100%" fill="#473F49" opacity={0.3} />
         <rect width="100%" height="100%" fill="url(#eye)" />
       </pattern>
     </defs>
